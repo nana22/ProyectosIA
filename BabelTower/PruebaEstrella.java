4ad9ec4c7b62e6581ab6b9ad147197067b223cb9
midoriHime <ka25vb@gmail.com>
2015-09-26 13:53:19 -0600

    Update PruebaEstrella.java

--- BabelTower/PruebaEstrella.java
+++ BabelTower/PruebaEstrella.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaestrella;
+
 import java.util.ArrayList;
 
 /**
@@ -11,9 +18,15 @@ public class PruebaEstrella {
      */
     public static void main(String[] args) {
         int[][] entrada = {{-1,5,5,5},{4,2,3,4},{1,2,4,3},{1,1,2,3},{2,1,4,3}};
-       // int[][] salida ={{-1,5,5,5},{3,4,4,2},{3,1,2,4},{1,2,3,1},{3,2,1,4}};
-        int[][] salida ={{5,5,5,-1},{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
-        A_Estrella aEstrella =new A_Estrella();
+     //  int[][] salida ={{-1,5,5,5},{3,4,4,2},{3,1,2,4},{1,2,3,1},{3,2,1,4}};
+     int[][] salida ={{-1,5,5,5},{1,2,3,4},{3,2,4,4},{1,1,2,3},{2,1,4,3}};
+//  int[][] salida ={{-1,5,5,5},{1,2,4,3},{4,2,3,4},{1,1,2,3},{2,1,4,3}};    
+//  int[][] salida ={{-1,5,5,5},{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
+    
+     // int[][] salida ={{-1,5,5,5},{1,2,4,3},{1,2,4,3},{1,2,4,3},{1,2,4,3}};
+      
+      // int[][] salida ={{-1,5,5,5},{4,1,1,2},{2,2,1,1},{3,4,2,4},{4,3,3,3}};
+        AEstrella aEstrella =new AEstrella();
         Tower nodo = aEstrella.A_Estrella(entrada,salida);
         if(nodo!=null){
         ArrayList<Tower> lista = new ArrayList<Tower>();
